c33dbde726ba30aae010e4693d8f218805a69c97
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2022-03-10 10:51:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DedEx
+
+Bolder Games's Game Jammers' Game Jam Jam Game
+
+Warehouse worker/delivery man's redemption arc
+
+Developed with Unreal Engine 4
---
+++ Source/DedEx/DedExGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DedExGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDEX_API ADedExGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
